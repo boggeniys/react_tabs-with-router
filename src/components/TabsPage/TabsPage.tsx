cd2c5dcb2c55777eb337b4adad2f728c053fa20d
Evgenu <bogdanovevgenij87@gmail.com>
2026-06-12 17:43:43 +0300

    finaly fixed

--- src/components/TabsPage/TabsPage.tsx
+++ src/components/TabsPage/TabsPage.tsx
@@ -1,61 +1,38 @@
 import React from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { TabType } from '../../types/Tab';
-import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
-import classNames from 'classnames';
+import { Link, useParams } from 'react-router-dom';
+import { Tab } from '../../types/Tab';
 
 interface Props {
-  tabs: TabType[];
+  tabs: Tab[];
 }
 
 export const TabsPage: React.FC<Props> = ({ tabs }) => {
   const { tabId } = useParams();
-  const navigate = useNavigate();
 
-  const activeTabIndex = tabs.findIndex(tab => tab.id === tabId);
-  const selectedIndex = activeTabIndex !== -1 ? activeTabIndex : 0;
-
-  // Проверяем: если tabId передан в URL, то он ОБЯЗАТЕЛЬНО должен быть найден в массиве tabs
-  const isTabValid = tabId ? activeTabIndex !== -1 : false;
-
-  const handleSelect = (index: number) => {
-    const clickedTab = tabs[index];
-    if (clickedTab) {
-      navigate(`/tabs/${clickedTab.id}`, { replace: true });
-    }
-  };
+  const activeTab = tabs.find(tab => tab.id === tabId);
 
   return (
     <div className="container">
       <h1 className="title">Tabs page</h1>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={tabId === tab.id ? 'is-active' : ''}
+            >
+              <Link to={`/tabs/${tab.id}`} replace>
+                {tab.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-      <Tabs selectedIndex={selectedIndex} onSelect={handleSelect}>
-        <div className="tabs is-boxed">
-          <TabList className="">
-            {tabs.map(tab => (
-              <Tab
-                key={tab.id}
-                data-cy="Tab"
-                className={classNames({ 'is-active': tabId === tab.id })}
-              >
-                <Link to={`/tabs/${tab.id}`} replace>
-                  {tab.title}
-                </Link>
-              </Tab>
-            ))}
-          </TabList>
-        </div>
-
-        {tabs.map(tab => (
-          <TabPanel
-            className="block"
-            data-cy="TabContent"
-            key={tab.id}
-          >
-            {isTabValid ? tab.content : 'Please select a tab'}
-          </TabPanel>
-        ))}
-      </Tabs>
+      <div className="block" data-cy="TabContent">
+        {activeTab ? activeTab.content : 'Please select a tab'}
+      </div>
     </div>
   );
 };
--- src/types/Tab.ts
+++ src/types/Tab.ts
@@ -1,4 +1,4 @@
-export interface TabType {
+export interface Tab {
   id: string;
   title: string;
   content: string;
